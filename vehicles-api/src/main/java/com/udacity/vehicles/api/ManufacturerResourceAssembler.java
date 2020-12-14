1fe20bc11b7663eff877385e7e41e143b6940631
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2020-12-14 10:08:34 +0100

    Clean code

--- vehicles-api/src/main/java/com/udacity/vehicles/api/ManufacturerResourceAssembler.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/ManufacturerResourceAssembler.java
@@ -1,17 +1,10 @@
 package com.udacity.vehicles.api;
 
-import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.manufacturer.Manufacturer;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-/**
- * Maps the CarController to the Car class using HATEOAS
- */
 @Component
 public class ManufacturerResourceAssembler implements ResourceAssembler<Manufacturer, Resource<Manufacturer>> {
 
